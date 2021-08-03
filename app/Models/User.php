8ec8ef0e8ff7f2a68cdb889552518cb36dea6f83
TeamCGSistemas <ivancgsistemas@gmail.com>
2021-08-02 21:23:17 -0500

    Ivan:imagen_usuario

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function adminlte_image(){
+        return 'https://picsum.photos/200/300';         
+    }
 }
